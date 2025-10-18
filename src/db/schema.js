5f64325130b488cef2cbd8171e7a05ee4452371a
Sai Balaji K <ksbalaji2000@outlook.com>
2025-10-18 09:15:12 +0530

    Added schema

--- src/db/schema.js
+++ src/db/schema.js
@@ -1,1 +1,9 @@
 const {text,uuid,varchar} = require("drizzle-orm/pg-core")
+const userTable = {
+    id: uuid().primaryKey().defaultRandom(),
+    userName: varchar({length:80}).notNull(),
+    email: text().notNull(),
+    salt: text().notNull(),
+    hashedPassword: text().notNull()
+}
+module.exports = {userTable}
